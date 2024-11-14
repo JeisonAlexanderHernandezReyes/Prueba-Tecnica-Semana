4c38768833ca209f92939609ca25cac3e76b98fb
Jeison Alexander Hernández Reyes <jeison.hernandez@pgd.com.co>
2024-11-13 20:38:05 -0500

    Eliminar lógica de autenticación en NavBar y simplificar enlaces de navegación

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { googleLogout } from '@react-oauth/google';
 
 const NavBar = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  const handleLogout = () => {
-    // Cierra sesión con Google
-    googleLogout();
-
-    // Cambia el estado de autenticación
-    setIsAuthenticated(false);
-
-    // Lógica adicional (opcional): redirigir al usuario, limpiar tokens, etc.
-    console.log('Sesión cerrada correctamente.');
-  };
-
   return (
     <nav className="bg-gray-900 text-white shadow-lg">
       <div className="container mx-auto px-4 py-2 flex justify-between items-center">
@@ -29,35 +15,27 @@ const NavBar = () => {
         {/* Menú */}
         <ul className="flex space-x-6">
           <li>
-            <Link to="/" className="hover:text-blue-400 transition duration-300">
-              Home Post
+            <Link
+              to="/"
+              className="hover:text-blue-400 transition duration-300"
+            >
+              Home
             </Link>
           </li>
-          {!isAuthenticated ? (
-            <li>
-              <Link
-                to="/login"
-                className="hover:text-blue-400 transition duration-300"
-              >
-                Login
-              </Link>
-            </li>
-          ) : (
-            <li>
-              <button
-                onClick={handleLogout}
-                className="hover:text-blue-400 transition duration-300"
-              >
-                Cerrar Sesión
-              </button>
-            </li>
-          )}
+          <li>
+            <Link
+              to="/login"
+              className="hover:text-blue-400 transition duration-300"
+            >
+              Ingresar
+            </Link>
+          </li>
           <li>
             <Link
               to="/users"
               className="hover:text-blue-400 transition duration-300"
             >
-              {isAuthenticated ? 'Mostrar Usuarios' : ''}
+              Mostrar Usuarios
             </Link>
           </li>
         </ul>
